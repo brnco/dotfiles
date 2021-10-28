04f5ae65094236663e2400e3a838da615b91b00e
brnco <brendan.emerson.coates@gmail.com>
2021-10-28 14:52:33 -0700

    add dwm, xinitrc, moc to sync script

--- sync.sh
+++ sync.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
+echo "starting"
 alacritty=$HOME"/.config/alacritty/alacritty.yml"
 alacrittybak="$alacritty.bak"
 bashrc=$HOME"/.bashrc"
 bashrcbak="$bashrc.bak"
 nvim=$HOME"/.config/nvim/init.vim"
 nvimbak="$nvim.bak"
+moc_config=$HOME"./moc/config"
+moc_themes=$HOME"/.moc/themes/"
+xinitrc=$HOME"/.xinitrc"
+dwm="/usr/src/dwm"
+vifm=$HOME"/.config/vifm"
 
 function get_current_dotfiles {
 	if [ -f "${alacritty}" ]; then
-		cp "${alacritty}" "/home/brnco/devs/dotfiles/alacritty/alacritty.yml"
+		cp "${alacritty}" "/home/brnco/code/dotfiles/alacritty/alacritty.yml"
 	fi
 
 	if [ -f "${bashrc}" ]; then
-		cp "${bashrc}" "/home/brnco/devs/dotfiles/bash/.bashrc"
+		cp "${bashrc}" "/home/brnco/code/dotfiles/bash/.bashrc"
 	fi
 
 	if [ -f "${nvim}" ]; then
-		cp "${nvim}" "/home/brnco/devs/dotfiles/nvim/init.vim"
+		cp "${nvim}" "/home/brnco/code/dotfiles/nvim/init.vim"
 	fi
+
+    if [ -f "${moc_config}" ]; then
+        cp "${moc_config}" "/home/brnco/code/dotfiles/moc"
+    fi
+
+    if [ -f "${moc_themes}" ]; then
+        cp -R "${moc_themes}" "/home/brnco/code/dotfiles/moc/"
+    fi
+
+    if [ -f "${xinitrc}" ]; then
+        cp "${xinitrc}" "/home/brnco/code/dotfiles"
+    fi
+
+    if [ -f "${dwm}" ]; then
+        cp -R "${dwm}" "/home/brnco/code/dotfiles/dwm/"
+    fi
+
+    if [ -f "${vifm}" ]; then
+        cp "${vifm}" "/home/brnco/code/dotfiles/vifm"
+    fi
 }
 
 function send_repo_dotfiles {
-	cp "/home/brnco/devs/dotfiles/alacritty/alacritty.yml" "${alacritty}"
-	cp "/home/brnco/devs/dotfiles/bash/.bashrc" "${bashrc}"
-	cp "/home/brnco/devs/dotfiles/nvim/init.vim" "${nvim}"
+	cp "/home/brnco/code/dotfiles/alacritty/alacritty.yml" "${alacritty}"
+	cp "/home/brnco/code/dotfiles/bash/.bashrc" "${bashrc}"
+	cp "/home/brnco/code/dotfiles/nvim/init.vim" "${nvim}"
+    cp "/home/brnco/code/dotfiles/moc/config" "${moc_config}"
+    cp -R "/home/brnco/code/dotfiles/moc/themes/" "${moc_themes}"
 }
 function backup_current_dotfiles {
 	if [ ! -f "${alacrittybak}" ]; then
@@ -41,8 +69,9 @@ function backup_current_dotfiles {
 
 get=false
 send=false
-
+echo "here"
 while getopts "gs:" opt; do
+    echo "there"
 	case $opt in
 		g) get=true ;;
 		s) send=true ;;
