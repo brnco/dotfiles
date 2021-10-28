364f4d05a08ee4c023982c7cacea0c1501258f9b
brnco <brendan.emerson.coates@gmail.com>
2021-10-28 15:07:54 -0700

    change variable declarations for filepaths at top of sync

--- sync.sh
+++ sync.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 echo "starting"
-alacritty=$HOME"/.config/alacritty/alacritty.yml"
-alacrittybak="$alacritty.bak"
+dotfiles=$HOME"/code/dotfiles/"
+alacritty=$HOME"/.config/alacritty/"
+alacritty_git=$dotfiles"alacritty/"
 bashrc=$HOME"/.bashrc"
-bashrcbak="$bashrc.bak"
-nvim=$HOME"/.config/nvim/init.vim"
-nvimbak="$nvim.bak"
-moc_config=$HOME"./moc/config"
-moc_themes=$HOME"/.moc/themes/"
+bashrc_git=$dotfiles"bash/"
+nvim=$HOME"/.config/nvim/"
+nvim_git=$dotfiles"nvim/"
+moc=$HOME"./moc/"
+moc_git=$dotfiles"moc/"
 xinitrc=$HOME"/.xinitrc"
-dwm="/usr/src/dwm"
-vifm=$HOME"/.config/vifm"
-
+xinitrc_git=$dotfiles
+dwm="/usr/src/dwm/"
+dwm_git=$dotfiles"dwm/"
+vifm=$HOME"/.config/vifm/"
+vifm_git=$dotfiles"vifm/"
+powerline=$HOME"/.config/powerline/"
+powerline_git=$dotfiles"powerline/"
 function get_current_dotfiles {
 	if [ -f "${alacritty}" ]; then
-		cp "${alacritty}" "/home/brnco/code/dotfiles/alacritty/alacritty.yml"
+		cp -R "${alacritty}" "${alacritty_git}"
 	fi
 
 	if [ -f "${bashrc}" ]; then
-		cp "${bashrc}" "/home/brnco/code/dotfiles/bash/.bashrc"
+		cp "${bashrc}" "${bashrc_git}"
 	fi
 
 	if [ -f "${nvim}" ]; then
-		cp "${nvim}" "/home/brnco/code/dotfiles/nvim/init.vim"
+		cp -R "${nvim}" "${nvim_git}"
 	fi
 
-    if [ -f "${moc_config}" ]; then
-        cp "${moc_config}" "/home/brnco/code/dotfiles/moc"
+    if [ -f "${moc}" ]; then
+        cp -R "${moc}" "${moc_git}"
     fi
 
-    if [ -f "${moc_themes}" ]; then
-        cp -R "${moc_themes}" "/home/brnco/code/dotfiles/moc/"
+    if [ -f "${powerline}" ]; then
+        cp -R "${powerline}" "${powerline_git}"
     fi
 
     if [ -f "${xinitrc}" ]; then
-        cp "${xinitrc}" "/home/brnco/code/dotfiles"
+        cp "${xinitrc}" "${xinitrc_git}"
     fi
 
     if [ -f "${dwm}" ]; then
-        cp -R "${dwm}" "/home/brnco/code/dotfiles/dwm/"
+        cp -R "${dwm}" "${dwm_git}"
     fi
 
     if [ -f "${vifm}" ]; then
-        cp "${vifm}" "/home/brnco/code/dotfiles/vifm"
+        cp -R "${vifm}" "${vifm_git}"
     fi
 }
 
@@ -53,25 +58,11 @@ function send_repo_dotfiles {
     cp "/home/brnco/code/dotfiles/moc/config" "${moc_config}"
     cp -R "/home/brnco/code/dotfiles/moc/themes/" "${moc_themes}"
 }
-function backup_current_dotfiles {
-	if [ ! -f "${alacrittybak}" ]; then
-		cp "${alacritty}" "${alacrittybak}"
-	fi
-
-	if [ ! -f "${bashrcbak}" ]; then
-		cp "${bashrc}" "${bashrcbak}"
-	fi
-
-	if [ ! -f "${nvimbak}" ]; then
-		cp "${nvim}" "${nvimbak}"
-	fi
-}
 
 get=false
 send=false
-echo "here"
+
 while getopts "gs:" opt; do
-    echo "there"
 	case $opt in
 		g) get=true ;;
 		s) send=true ;;
@@ -88,6 +79,5 @@ fi
 if [ "$send" = true ];
 then
 	echo "send"
-	backup_current_dotfiles
 	send_repo_dotfiles
 fi
